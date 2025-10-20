9a7d3cd543e01f5acf5f103a16d3f52366a27899
Kim Long Ngo <ngokimlong2007@gmail.com>
2025-10-19 21:15:30 -0400

    Init Commit

---
+++ src/main/java/com/kimlngo/userserv/UserServApplication.java
@@ -0,0 +1,13 @@
+package com.kimlngo.userserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimlngo/userserv/UserServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimlngo.userserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
